83915907f328edf865cb4c445495d865a6205fdb
lzbgithubcode <1835064412@qq.com>
2017-02-08 16:23:27 +0800

    初始化

---
+++ LZBMVVMDemo/LZBMVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZBMVVMDemo
+//
+//  Created by zibin on 17/2/8.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
