d61e6ddda5d42e5a53318c7caf9f45fab6e9e760
lzbgithubcode <1835064412@qq.com>
2017-02-08 17:22:40 +0800

    基本数据

---
+++ LZBMVVMDemo/LZBMVVMDemo/Common/NetWork/NetWorkResquestTool.m
@@ -0,0 +1,228 @@
+//
+//  NetWorkResquestTool.m
+//  LZBYKLive
+//
+//  Created by zibin on 16/8/30.
+//  Copyright © 2016年 刘子彬（更多项目源码分享平台“开发者源代码” 微信号：developerCode 作者简介：iOS开发者，喜爱交流分享）. All rights reserved.
+//
+
+#import "NetWorkResquestTool.h"
+#import "LZBFuncTools.h"
+#import <AFNetworking/AFHTTPSessionManager.h>
+
+//响应接受类型
+#define kResponseAcceptableContentTypes   [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html",@"text/plain",@"charset=utf-8", nil]
+
+static AFHTTPSessionManager *_resquestManger;
+
+@interface NetWorkResquestTool()
+
+@property (nonatomic, strong) Class responseClass;
+@end
+
+@implementation NetWorkResquestTool
+
++ (AFHTTPSessionManager *)shareResquestManger
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        if(_resquestManger == nil)
+            _resquestManger = [AFHTTPSessionManager manager];
+    });
+    _resquestManger.requestSerializer = [AFHTTPRequestSerializer serializer];
+    _resquestManger.responseSerializer = [AFJSONResponseSerializer serializer];
+    _resquestManger.responseSerializer.acceptableContentTypes = kResponseAcceptableContentTypes;
+    return _resquestManger;
+}
+
+- (instancetype)init
+{
+  if(self = [super init])
+  {
+      //设置默认值
+      self.timeOut = 10.0;
+      self.responseQueue = dispatch_get_main_queue();
+      self.responseDataType = NetWorkResponseDataType_Default;
+  }
+    return self;
+}
+
+#pragma mark - 网络请求
+- (NSURLSessionDataTask *)httpGetWithModel:(BaseResquestModel *)requestModel
+                             ResponseClass:(Class)responseClass
+                            sucessResponse:(sucessResponseBlock)sucessBlock
+                              failResponse:(failResponseBlock)failBlock
+{
+    [self resquestProcess:requestModel responseClass:responseClass];
+    
+    return [self baseGetHttpWithURL:requestModel.url parameters:[requestModel modelToBaseResquestModel] sucessResponse:sucessBlock failResponse:failBlock];
+}
+
+- (NSURLSessionDataTask *)httpPostWithModel:(BaseResquestModel *)requestModel
+                              ResponseClass:(Class)responseClass
+                             sucessResponse:(sucessResponseBlock)sucessBlock
+                               failResponse:(failResponseBlock)failBlock
+{
+   [self resquestProcess:requestModel responseClass:responseClass];
+    return [self basePostHttpWithURL:[requestModel url] parameters:[requestModel modelToBaseResquestModel] sucessResponse:sucessBlock failResponse:failBlock];
+}
+
+- (void)resquestProcess:(BaseResquestModel *)requestModel responseClass:(Class)responseClass
+{
+    _resquestModel = requestModel;
+    self.responseClass= responseClass;
+}
+#pragma mark - 基础网络请求
+/**
+ *  get-基本网络请求
+ *
+ *  @param url         请求地址
+ *  @param paramDicts  请求参数
+ *  @param sucessBlock 请求成功
+ *  @param failBlock   请求失败
+ *
+ *  @return
+ */
+- (NSURLSessionDataTask *)baseGetHttpWithURL:(NSString *)url
+                                  parameters:(NSDictionary *)paramDicts
+                              sucessResponse:(sucessResponseBlock)sucessBlock
+                                failResponse:(failResponseBlock)failBlock{
+    
+  AFHTTPSessionManager *httpManger = [self configHttpBaseInfoWithURL:url withParamsDict:paramDicts httpType:@"GET-请求方式"];
+   
+    NSURLSessionDataTask *task = [httpManger GET:url parameters:paramDicts success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+        [self resultProcess:responseObject error:nil sucessResponse:sucessBlock failResponse:failBlock];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [self resultProcess:nil error:error sucessResponse:sucessBlock failResponse:failBlock];
+    }];
+    return task;
+}
+
+/**
+ *  post-基本网络请求
+ *
+ *  @param url         请求地址
+ *  @param paramDicts  请求参数
+ *  @param sucessBlock 请求成功
+ *  @param failBlock   请求失败
+ *
+ *  @return
+ */
+- (NSURLSessionDataTask *)basePostHttpWithURL:(NSString *)url
+                                  parameters:(NSDictionary *)paramDicts
+                              sucessResponse:(sucessResponseBlock)sucessBlock
+                                failResponse:(failResponseBlock)failBlock
+{
+     AFHTTPSessionManager *httpManger = [self configHttpBaseInfoWithURL:url withParamsDict:paramDicts httpType:@"post请求方式"];
+    NSURLSessionDataTask *task = [httpManger POST:url parameters:paramDicts success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+        [self resultProcess:responseObject error:nil sucessResponse:sucessBlock failResponse:failBlock];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+         [self resultProcess:nil error:error sucessResponse:sucessBlock failResponse:failBlock];
+    }];
+    return task;
+}
+
+- (AFHTTPSessionManager *)configHttpBaseInfoWithURL:(NSString *)url withParamsDict:(NSDictionary *)paramDicts  httpType:(NSString *)typeSting
+{
+    //检测URL
+    url = [self checkResquestURLWithURL:url];
+    NSLog(@"request:\n[\n requestType:%@\n  url:%@\n  params:%@\n]",typeSting,url,paramDicts);
+    
+    //设置请求基本信息
+    AFHTTPSessionManager *httpManger = [NetWorkResquestTool shareResquestManger];
+    if(![self.responseQueue isEqual:dispatch_get_main_queue()])
+        httpManger.completionQueue = self.responseQueue;
+    httpManger.requestSerializer.timeoutInterval = self.timeOut;
+    //增加请求头
+    [self addResquestHeadDictionary:self.resquestDefaultHeadDict WithRequestSerializer:httpManger.requestSerializer];
+    
+    return httpManger;
+}
+
+#pragma mark- 请求结果处理
+- (void)resultProcess:(id)responseDict
+                error:(NSError *)error
+       sucessResponse:(sucessResponseBlock)sucessBlock
+         failResponse:(failResponseBlock)failBlock
+{
+   if(error == nil)
+   {
+       if(responseDict == nil)
+       {
+           LZBCustomLog(@"网络请求结果为:%@",responseDict);
+           return;
+       }
+       NSError *jsonError;
+       _responseModel = [MTLJSONAdapter modelOfClass:self.responseClass fromJSONDictionary:responseDict error:&jsonError];
+       if(_responseModel == nil||jsonError)
+       {
+           LZBCustomLog(@"\n序列化失败：%@",jsonError);
+           return;
+       }
+       if(sucessBlock != nil)
+       {
+          dispatch_async(self.responseQueue, ^{
+              sucessBlock(_responseModel);
+          });
+       }
+   }
+   else
+   {
+       if(failBlock != nil)
+       {
+           dispatch_async(self.responseQueue, ^{
+               failBlock(error);
+           });
+       }
+           
+   }
+}
+
+
+#pragma mark - 请求参数配置
+- (void)addResquestHeadDictionary:(NSDictionary *)headDict WithRequestSerializer:(AFHTTPRequestSerializer *)requestSerializer
+{
+    [headDict enumerateKeysAndObjectsUsingBlock:^(NSString  *key, NSString  *obj, BOOL * _Nonnull stop) {
+        if(key.length == 0 || obj.length == 0) *stop = YES;
+        [requestSerializer setValue:obj forHTTPHeaderField:key];
+    }];
+}
+
+- (NSString *)checkResquestURLWithURL:(NSString *)url
+{
+   if(url == nil)
+   {
+       NSLog(@"URL为空");
+       return @"htttp://www.baidu.com";
+   }
+   else
+   {
+      if(isContainChinese(url))
+      {
+          url = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+      }
+       return url;
+   }
+}
+
+#pragma mark - set/get
+ - (NSMutableDictionary *)resquestDefaultHeadDict
+{
+    if(_resquestDefaultHeadDict == nil)
+    {
+        _resquestDefaultHeadDict = [NSMutableDictionary dictionary];
+    }
+    return _resquestDefaultHeadDict;
+}
+
+- (Class)responseClass
+{
+  if(_responseClass == nil)
+  {
+      _responseClass = [BaseResponseModel class];
+  }
+    return _responseClass;
+}
+
+
+@end
